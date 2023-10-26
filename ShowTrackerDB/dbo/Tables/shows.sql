7813831621e68d3d028e4390d1daf66bf42121f5
Nick Mohale <52918728+nickmohale-collab@users.noreply.github.com>
2023-10-26 17:03:21 +0200

    showTracker Database

---
+++ ShowTrackerDB/dbo/Tables/shows.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE shows (
+  show_id SERIAL PRIMARY KEY,
+  title VARCHAR(255) NOT NULL,
+  imdb_id VARCHAR(20) UNIQUE,
+  description TEXT,
+  poster_url VARCHAR(255),
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
